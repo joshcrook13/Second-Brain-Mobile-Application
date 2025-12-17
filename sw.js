a2d407792500178d4c047dbdc59b26f4b40aa108
joshcrook13 <josh@crook.uk>
2025-12-17 17:39:55 +0000

    dashboard updates

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // --- CONFIGURATION ---
 // CHANGE THIS NUMBER whenever you update your app
-const CACHE_NAME = 'josh-os-v16'; 
+const CACHE_NAME = 'josh-os-v17'; 
 
 // Add any new files you create here
 const urlsToCache = [
