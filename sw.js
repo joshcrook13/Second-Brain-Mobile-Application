5cb99c960002adcbe365abef5a38870493d73884
joshcrook13 <josh@crook.uk>
2025-12-17 15:16:09 +0000

    Inital Commit

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'second-brain-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './login.html',
+  './apps/restaurantindex.html',
+  './apps/finance.html',
+  './apps/Personalcrm.html',
+  './apps/todo.html',
+  './apps/journalapp.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
