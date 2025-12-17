a6b6524d1c7b7f31bef6841fefa47a0459e98f63
joshcrook13 <josh@crook.uk>
2025-12-17 17:19:03 +0000

    all g

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // --- CONFIGURATION ---
 // CHANGE THIS NUMBER whenever you update your app
-const CACHE_NAME = 'josh-os-v15'; 
+const CACHE_NAME = 'josh-os-v16'; 
 
 // Add any new files you create here
 const urlsToCache = [
