29eb1ff9cf42703a54ac8495c1345a28d9a59e2a
joshcrook13 <josh@crook.uk>
2025-12-18 20:48:52 +0000

    updated ui for only app view

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // --- CONFIGURATION ---
 // CHANGE THIS NUMBER whenever you update your app
-const CACHE_NAME = 'josh-os-v17'; 
+const CACHE_NAME = 'josh-os-v18'; 
 
 // Add any new files you create here
 const urlsToCache = [
