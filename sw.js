8e348c76e5b84ee29b2248b33d1b8baa7562d653
joshcrook13 <josh@crook.uk>
2025-12-18 21:44:46 +0000

    tiles spacing edits

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // --- CONFIGURATION ---
 // CHANGE THIS NUMBER whenever you update your app
-const CACHE_NAME = 'josh-os-v28'; 
+const CACHE_NAME = 'josh-os-v29'; 
 
 // Add any new files you create here
 const urlsToCache = [
