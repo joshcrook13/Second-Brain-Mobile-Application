aeab3340aa3f5e6cf35e4a200d93b9335bd6fe4b
joshcrook13 <josh@crook.uk>
2025-12-18 21:36:13 +0000

    fitting ui on dashboard perfectly

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // --- CONFIGURATION ---
 // CHANGE THIS NUMBER whenever you update your app
-const CACHE_NAME = 'josh-os-v27'; 
+const CACHE_NAME = 'josh-os-v28'; 
 
 // Add any new files you create here
 const urlsToCache = [
